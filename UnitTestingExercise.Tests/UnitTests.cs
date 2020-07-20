92f8673127404e8806e0b275749035903214c173
Andrew Sutton <ajsutton94@gmail.com>
2020-07-20 09:12:06 -0500

    finished exercise

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,62 +6,71 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(4, 5, 6, 15)]
+        [InlineData(300, 300, 200, 800)]
+        [InlineData(-1, 5, -4, 0)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            var calculator = new UnitTestMethods();
             //Act
-
+            var actual = calculator.Add(num1, num2, num3);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
-        public void Subtract(int minuend, int subtrhend, int expected)
+        [InlineData(5, 4, 1)]
+        [InlineData(50, 20, 30)]
+        [InlineData(-1, 14, -15)]
+        public void Subtract(int minuend, int subtrahend, int expected)
         {
             //Arrange
-
+            var calculator = new UnitTestMethods();
             //Act
-
+            var actual = calculator.Subtract(minuend, subtrahend);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(9, 9, 81)]
+        [InlineData(2, 3, 6)]
+        [InlineData(2, -1, -2)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
-
+            var calculator = new UnitTestMethods();
             //Act
-
+            var actual = calculator.Multiply(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(9, 3, 3)]
+        [InlineData(12, 5, 2)]
+        [InlineData(100, 0, -1)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
-
+            var calculator = new UnitTestMethods();
             //Act
-
+            var actual = calculator.Divide(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
-        [Fact]
-        public void YourMethodName()
+        [Theory]
+        [InlineData("string", "tringsay")]
+        public void PigLatin(string str, string expected)
         {
             //Arrange
-
+            var tester = new UnitTestMethods();
             //Act
-
+            var actual = tester.PigLatin(str);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -1,26 +1,61 @@
 ﻿using System;
+using System.ComponentModel;
+
 namespace UnitTestingExercise
 {
     public class UnitTestMethods
     {
         // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
+        // Righ-click on Dependencies in your .Tests project and set your reference
 
 
 
         // Create an Add method that passes 3 integers
+        public int Add(int int1, int int2, int int3)
+        {
+            return int1 + int2 + int3;
+        }
 
         // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
+        // Keep track of which number is getting passed as minuend and subtrahend
+        public int Subtract(int minuend, int subtrahend)
+        {
+            return minuend - subtrahend;
+        }
 
         // Create a Multiply method that passes 2 integers
+        public int Multiply(int int1, int int2)
+        {
+            return int1 * int2;
+        }
 
         // Create a Divide method that passes 2 integers
-
+        public int Divide(int int1, int int2)
+        {
+            try
+            {
+                return int1 / int2;
+            }
+            catch
+            {
+                Console.WriteLine("Cannot divide by 0");
+                return -1;
+            }
+        }
 
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
+        public string PigLatin(string str)
+        {
+            char c = str[0];
+            string toReturn = str.Substring(1);
+            return $"{toReturn}{c}ay";
+        }
 
-        
+        public void PrintHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
     }
 }
